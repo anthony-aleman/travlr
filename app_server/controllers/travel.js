cfff0807a1abd5206c0a6b71e42d3539a2788c3c
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2025-05-24 21:02:58 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,7 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
     
 };
 
