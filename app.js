bf71f7e8de6a2318e0c240a72b9be6565a0e9ded
Anthony Aleman <58735466+anthony-aleman@users.noreply.github.com>
2025-06-16 00:58:31 -0400

    Add files via upload
    module 5 api

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+require('./app_api/database/db');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const aboutRouter = require('./app_server/routes/about');
+const contactRouter = require('./app_server/routes/contact');
+const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const roomsRouter = require('./app_server/routes/rooms');
+const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +32,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/rooms', roomsRouter);
+app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +54,17 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Custom Handlebars helper to check if the current page matches the active page
+hbs.registerHelper('activePage', function(page, options) {
+  const currentPage = options.data.root.activePage;
+  return currentPage === page ? 'selected' : '';
+});
+
+hbs.registerHelper('activePageFooter', function(page, options) {
+  const currentPage = options.data.root.activePage;
+  return currentPage === page ? 'active' : '';
 });
 
 module.exports = app;
